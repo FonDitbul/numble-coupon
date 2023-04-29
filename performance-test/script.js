131f12e4c885e7c1bd6ee0ad0ce6c72b4f06ddea
FonDitbul <niki7084@naver.com>
2023-04-29 21:50:33 +0900

    script 수정

--- performance-test/script.js
+++ performance-test/script.js
@@ -9,8 +9,8 @@ const requestPerGive = new Counter('request_per_give');
 const responsePerGive = new Counter('response_per_give');
 
 export const options = {
-  vus: 750,
-  duration: '30m',
+  vus: 500,
+  duration: '1h',
 };
 
 export default () => {
@@ -18,70 +18,70 @@ export default () => {
     plaintext: true,
   });
 
-  // 유저의 쿠폰 읽기
-  group('유저의 쿠폰 읽기', () => {
-    const randUser_0_100 = Math.floor(Math.random() * 101);
-    const userId = `user_${randUser_0_100}`;
-
-    const data = {
-      userId: userId,
-      take: 10,
-    };
-
-    const couponArrayResponse = client.invoke('user_coupon.UserCouponService/FindAll', data);
-  });
-
-  // 유저의 쿠폰 사용하기
-  group('유저의 쿠폰 사용하기', () => {
-    const randUser_0_100 = Math.floor(Math.random() * 101);
-    const userId = `user_${randUser_0_100}`;
-    const randProduct_0_100 = Math.floor(Math.random() * 101);
-    const productId = `product_${randProduct_0_100}`;
-
-    const data = {
-      userId: userId,
-      take: 10,
-    };
-
-    const userCouponArrayResponse = client.invoke('user_coupon.UserCouponService/FindAll', data);
-
-    if (userCouponArrayResponse.message.userCouponStorages.length > 0) {
-      const oneUserCoupon = userCouponArrayResponse.message.userCouponStorages[0];
-      const useData = {
-        id: oneUserCoupon.id,
-        userId: userId,
-        couponId: oneUserCoupon.couponId,
-        productId: productId,
-      };
-
-      const couponArrayResponse = client.invoke('user_coupon.UserCouponService/Use', useData);
-    }
-  });
-
-  group('유저의 쿠폰 수량 X 발급 받기', () => {
-    const couponArrayResponse = client.invoke('coupon.CouponService/FindAll', {});
-
-    const oneCoupon = couponArrayResponse.message.coupons.find((coupon) => coupon.type == 2); // 수량 x 쿠폰인 경우
-
-    const randUser_0_100 = Math.floor(Math.random() * 101);
-    const userId = `user_${randUser_0_100}`;
-
-    const data = {
-      userId: userId,
-      couponId: oneCoupon.id,
-    };
-
-    const couponGiveRes = client.invoke('user_coupon.UserCouponService/Give', data);
-
-    requestPerGive.add(1);
-    check(couponGiveRes, {
-      'status is OK': (r) => r && r.status === grpc.StatusOK,
-    });
-
-    if (couponGiveRes.status == 0) {
-      responsePerGive.add(1);
-    }
-  });
+  // // 유저의 쿠폰 읽기
+  // group('유저의 쿠폰 읽기', () => {
+  //   const randUser_0_100 = Math.floor(Math.random() * 101);
+  //   const userId = `user_${randUser_0_100}`;
+  //
+  //   const data = {
+  //     userId: userId,
+  //     take: 10,
+  //   };
+  //
+  //   const couponArrayResponse = client.invoke('user_coupon.UserCouponService/FindAll', data);
+  // });
+  //
+  // // 유저의 쿠폰 사용하기
+  // group('유저의 쿠폰 사용하기', () => {
+  //   const randUser_0_100 = Math.floor(Math.random() * 101);
+  //   const userId = `user_${randUser_0_100}`;
+  //   const randProduct_0_100 = Math.floor(Math.random() * 101);
+  //   const productId = `product_${randProduct_0_100}`;
+  //
+  //   const data = {
+  //     userId: userId,
+  //     take: 10,
+  //   };
+  //
+  //   const userCouponArrayResponse = client.invoke('user_coupon.UserCouponService/FindAll', data);
+  //
+  //   if (userCouponArrayResponse.message.userCouponStorages.length > 0) {
+  //     const oneUserCoupon = userCouponArrayResponse.message.userCouponStorages[0];
+  //     const useData = {
+  //       id: oneUserCoupon.id,
+  //       userId: userId,
+  //       couponId: oneUserCoupon.couponId,
+  //       productId: productId,
+  //     };
+  //
+  //     const couponArrayResponse = client.invoke('user_coupon.UserCouponService/Use', useData);
+  //   }
+  // });
+  //
+  // group('유저의 쿠폰 수량 X 발급 받기', () => {
+  //   const couponArrayResponse = client.invoke('coupon.CouponService/FindAll', {});
+  //
+  //   const oneCoupon = couponArrayResponse.message.coupons.find((coupon) => coupon.type == 2); // 수량 x 쿠폰인 경우
+  //
+  //   const randUser_0_100 = Math.floor(Math.random() * 101);
+  //   const userId = `user_${randUser_0_100}`;
+  //
+  //   const data = {
+  //     userId: userId,
+  //     couponId: oneCoupon.id,
+  //   };
+  //
+  //   const couponGiveRes = client.invoke('user_coupon.UserCouponService/Give', data);
+  //
+  //   requestPerGive.add(1);
+  //   check(couponGiveRes, {
+  //     'status is OK': (r) => r && r.status === grpc.StatusOK,
+  //   });
+  //
+  //   if (couponGiveRes.status == 0) {
+  //     responsePerGive.add(1);
+  //   }
+  // });
 
   group('유저의 쿠폰 수량 발급 받기', () => {
     const couponArrayResponse = client.invoke('coupon.CouponService/FindAll', {});
